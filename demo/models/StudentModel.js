f85b6cc2c474b1197d70d32514b9d83e8548d872
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-04-03 16:25:00 +0700

    demo

--- demo/models/StudentModel.js
+++ demo/models/StudentModel.js
@@ -8,7 +8,8 @@ var StudentSchema = mongoose.Schema(
       birthYear: Number,   //integer
       gender: String,
       gpa: Number,         //double
-      image: String
+      image: String,
+      city: String
    }
 );
 //declare model (to be used in routes - controllers)
