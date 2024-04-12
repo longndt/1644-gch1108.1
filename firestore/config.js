8562de3aa0ffa081f0c99c0a733ec86c8f5484cb
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-04-12 15:02:13 +0700

    firestore

--- firestore/config.js
+++ firestore/config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 
 const firebaseConfig = {
